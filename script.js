44fa8915ccb4af5c1934e0e81b5808565daefce5
DimitrisNtarl <dimitrisntarlagiannis@hotmail.com>
2023-05-28 23:24:17 +0300

    extra credit

--- script.js
+++ script.js
@@ -22,4 +22,37 @@ function createNewGrid() {
     }
   }
 
+  const gridItems = document.querySelectorAll('.grid-item');
+  gridItems.forEach(item => {
+    item.addEventListener('mouseenter', handleMouseEnter);
+  });
+    
+  function handleMouseEnter(event) {
+    const currentColor = getComputedStyle(event.target).backgroundColor;
+    const colorParts = currentColor.match(/\d+/g);
+    const red = parseInt(colorParts[0]);
+    const green = parseInt(colorParts[1]);
+    const blue = parseInt(colorParts[2]);
+
+    let newRed, newGreen, newBlue;
+
+    if (red === 255 && green === 255 && blue === 255) {
+      newRed = getRandomColorValue();
+      newGreen = getRandomColorValue();
+      newBlue = getRandomColorValue();
+    } else if (red === 0 && green === 0 && blue === 0){
+      return;
+    } else {
+      newRed = Math.max(0, red - 25.5);
+      newGreen = Math.max(0, green - 25.5);
+      newBlue = Math.max(0, blue - 25.5);
+    }
+
+    const newColor = `rgb(${newRed}, ${newGreen}, ${newBlue})`;
+    event.target.style.backgroundColor = newColor;
+  }
+
+  function getRandomColorValue() {
+    return Math.floor(Math.random() * 256);
+  }
 };
